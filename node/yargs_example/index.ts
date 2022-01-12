1b86658703a5da9531fa44fc2d0358d8bbb8c92a
htlsne <htlsne23@gmail.com>
2022-01-12 22:13:35 +0900

    Update

--- node/yargs_example/index.ts
+++ node/yargs_example/index.ts
@@ -3,52 +3,18 @@ import { hideBin } from "yargs/helpers";
 
 yargs(hideBin(process.argv))
   .command(
-    "serve <arg1> [port]",
-    "start the server",
+    "* <arg1>",
+    "description",
     (yargs) => {
       return yargs
-        .positional("port", {
-          describe: "port to bind on",
-          default: 5000,
-        })
         .positional("arg1", {
-          describe: "port to bind on",
-          type: "number",
-        });
-    },
-    (argv) => {
-      if (argv.verbose) console.info(`start server on :${argv.port}`);
-      console.log(argv.port);
-      console.log(argv.arg1);
-    }
-  )
-  .command(
-    "$0 <arg2>",
-    "description",
-    (yargs) => {
-      return yargs
-        .positional("arg2", {
-          type: "number",
-        })
-        .demandOption(["arg2"]);
-    },
-    (argv) => {
-      console.log(argv.arg2);
-    }
-  )
-  .command(
-    "command3 <arg3>",
-    "description",
-    (yargs) => {
-      return yargs
-        .positional("arg3", {
           type: "string",
           demandOption: true,
         })
-        .demandOption(["arg3"]);
+        .demandOption(["arg1"]);
     },
     (argv) => {
-      console.log(argv.arg3);
+      console.log(argv.arg1);
     }
   )
   .parse();
