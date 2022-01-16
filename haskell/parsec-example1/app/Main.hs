f87f34beeb695f3de1f85a3e7399884eee753ca8
htlsne <htlsne23@gmail.com>
2022-01-16 11:46:16 +0900

    Update

--- haskell/parsec-example1/app/Main.hs
+++ haskell/parsec-example1/app/Main.hs
@@ -1,18 +1,30 @@
 module Main where
 
--- file: ch16/csv2.hs
 import Text.ParserCombinators.Parsec
+import Text.Parsec
 
+csvFile :: Parser [[String]]
 csvFile = endBy line eol
 
+line :: Parser [String]
 line = sepBy cell (char ',')
 
+cell :: Parser String
 cell = many (noneOf ",\n")
 
 eol = char '\n'
 
+numberStr :: Parser String
+numberStr = many1 digit
+
+number :: Parser Int
+number = read <$> numberStr
+
 parseCSV :: String -> Either ParseError [[String]]
 parseCSV input = parse csvFile "(unknown)" input
+
+parseNumber :: String -> Either ParseError Int
+parseNumber input = parse number "" input
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
