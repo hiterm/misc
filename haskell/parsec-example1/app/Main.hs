2c75df3fbdad79eb606847a05213f9ffe68fec3b
htlsne <htlsne23@gmail.com>
2022-01-16 11:22:05 +0900

    Update

--- haskell/parsec-example1/app/Main.hs
+++ haskell/parsec-example1/app/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+-- file: ch16/csv2.hs
+import Text.ParserCombinators.Parsec
+
+csvFile = endBy line eol
+
+line = sepBy cell (char ',')
+
+cell = many (noneOf ",\n")
+
+eol = char '\n'
+
+parseCSV :: String -> Either ParseError [[String]]
+parseCSV input = parse csvFile "(unknown)" input
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
