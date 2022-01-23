e73b0812a4265a0515d6ae84efaffeb7e831ba64
htlsne <htlsne23@gmail.com>
2022-01-23 15:36:50 +0900

    Create AST module

---
+++ haskell/arithmetic_expression/app/AST.hs
@@ -0,0 +1,19 @@
+module AST where
+
+data Expression
+  = IntegerLiteral Int
+  | BinaryExpression Operator Expression Expression
+  deriving (Show)
+
+data Operator
+  = Add
+  | Multiply
+  deriving (Show)
+
+eval :: Expression -> Int
+eval (IntegerLiteral n) = n
+eval (BinaryExpression op l r) = evalOperator op (eval l) (eval r)
+
+evalOperator :: Operator -> Int -> Int -> Int
+evalOperator Add = (+)
+evalOperator Multiply = (*)
--- haskell/arithmetic_expression/app/Main.hs
+++ haskell/arithmetic_expression/app/Main.hs
@@ -1,22 +1,6 @@
 module Main where
 
-data Expression
-  = IntegerLiteral Int
-  | BinaryExpression Operator Expression Expression
-  deriving (Show)
-
-data Operator
-  = Add
-  | Multiply
-  deriving (Show)
-
-eval :: Expression -> Int
-eval (IntegerLiteral n) = n
-eval (BinaryExpression op l r) = evalOperator op (eval l) (eval r)
-
-evalOperator :: Operator -> Int -> Int -> Int
-evalOperator Add = (+)
-evalOperator Multiply = (*)
+import AST
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
