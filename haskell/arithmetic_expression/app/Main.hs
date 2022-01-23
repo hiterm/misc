fc12328d7736c48873d6acd49a460fb65556f32f
htlsne <htlsne23@gmail.com>
2022-01-23 14:49:17 +0900

    Evaluate AST

--- haskell/arithmetic_expression/app/Main.hs
+++ haskell/arithmetic_expression/app/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+data Expression
+  = IntegerLiteral Int
+  | BinaryExpression Operator Expression Expression
+  deriving (Show)
+
+data Operator
+  = Add
+  | Multiply
+  deriving (Show)
+
+eval :: Expression -> Int
+eval (IntegerLiteral n) = n
+eval (BinaryExpression Add l r) = eval l + eval r
+eval (BinaryExpression Multiply l r) = eval l * eval r
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
