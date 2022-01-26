09939d970c9ecc89fc52e65873f9eccab6b2127a
htlsne <htlsne23@gmail.com>
2022-01-26 19:14:20 +0900

    Write parser (wip)

--- haskell/arithmetic-expression/app/Main.hs
+++ haskell/arithmetic-expression/app/Main.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import AST
+import Data.Text
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void Text
+
+number :: Parser Int
+number = L.decimal
+
+expression :: Parser (Int, [(Char, Int)])
+expression = (,) <$> number <*> many ((,) <$> char '+' <*> number)
 
 main :: IO ()
 main = print $ eval $ BinaryExpression Add (IntegerLiteral 1) (IntegerLiteral 2)
