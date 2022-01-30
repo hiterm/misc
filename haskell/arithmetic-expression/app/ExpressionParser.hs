eb07ff984f65060be2e99e1aaeb50d5fc393f566
htlsne <htlsne23@gmail.com>
2022-01-30 12:52:04 +0900

    Implement main

---
+++ haskell/arithmetic-expression/app/ExpressionParser.hs
@@ -0,0 +1,30 @@
+module ExpressionParser where
+
+import AST
+import Data.Text
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void Text
+
+number :: Parser Expression
+number = IntegerLiteral <$> L.decimal
+
+expression :: Parser Expression
+expression = Prelude.foldl f <$> l <*> r
+  where
+    l = term
+    r = many ((,) <$> char '+' <*> term)
+    f e1 (op, e2) = BinaryExpression Add e1 e2
+
+term :: Parser Expression
+term = Prelude.foldl f <$> l <*> r
+  where
+    l = factor
+    r = many ((,) <$> char '*' <*> factor)
+    f e1 (op, e2) = BinaryExpression Multiply e1 e2
+
+factor :: Parser Expression
+factor = number <|> (char '(' *> expression <* char ')')
--- haskell/arithmetic-expression/app/Main.hs
+++ haskell/arithmetic-expression/app/Main.hs
@@ -4,32 +4,15 @@ module Main where
 
 import AST
 import Data.Text
-import Data.Void
+import Data.Text.IO as IO
+import ExpressionParser
 import Text.Megaparsec
-import Text.Megaparsec.Char
-import qualified Text.Megaparsec.Char.Lexer as L
-
-type Parser = Parsec Void Text
-
-number :: Parser Expression
-number = IntegerLiteral <$> L.decimal
-
-expression :: Parser Expression
-expression = Prelude.foldl f <$> l <*> r
-  where
-    l = term
-    r = many ((,) <$> char '+' <*> term)
-    f e1 (op, e2) = BinaryExpression Add e1 e2
-
-term :: Parser Expression
-term = Prelude.foldl f <$> l <*> r
-  where
-    l = factor
-    r = many ((,) <$> char '*' <*> factor)
-    f e1 (op, e2) = BinaryExpression Multiply e1 e2
-
-factor :: Parser Expression
-factor = number <|> (char '(' *> expression <* char ')')
 
 main :: IO ()
-main = print $ eval $ BinaryExpression Add (IntegerLiteral 1) (IntegerLiteral 2)
+main = do
+  input <- IO.getLine
+  let exp = runParser (expression <* eof) "" input
+  let result = eval <$> exp
+  case result of
+    Right n -> print n
+    Left e -> print e
