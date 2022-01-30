5737bd1fbef2eedd1377f4b47ffa74d148090235
htlsne <htlsne23@gmail.com>
2022-01-30 14:15:56 +0900

    Parse +-*/

--- haskell/arithmetic-expression/app/ExpressionParser.hs
+++ haskell/arithmetic-expression/app/ExpressionParser.hs
@@ -6,6 +6,7 @@ import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
+import Data.Functor
 
 type Parser = Parsec Void Text
 
@@ -16,15 +17,15 @@ expression :: Parser Expression
 expression = Prelude.foldl f <$> l <*> r
   where
     l = term
-    r = many ((,) <$> char '+' <*> term)
-    f e1 (op, e2) = BinaryExpression Add e1 e2
+    r = many ((,) <$> ((char '+' $> Add) <|> (char '-' $> Subtract)) <*> term)
+    f e1 (op, e2) = BinaryExpression op e1 e2
 
 term :: Parser Expression
 term = Prelude.foldl f <$> l <*> r
   where
     l = factor
-    r = many ((,) <$> char '*' <*> factor)
-    f e1 (op, e2) = BinaryExpression Multiply e1 e2
+    r = many ((,) <$> ((char '*' $> Multiply) <|> (char '/' $> Division)) <*> factor)
+    f e1 (op, e2) = BinaryExpression op e1 e2
 
 factor :: Parser Expression
 factor = number <|> (char '(' *> expression <* char ')')
